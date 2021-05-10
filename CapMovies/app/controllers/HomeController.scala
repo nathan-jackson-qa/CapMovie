f81571ce5afdd7b78126c8207a67b9dcb4add324
Simon Forster <sforster@academytrainee.com>
2021-05-10 10:41:09 +0100

    Starter setup

--- CapMovies/app/controllers/HomeController.scala
+++ CapMovies/app/controllers/HomeController.scala
@@ -7,7 +7,7 @@ import play.api.mvc._
 class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    Ok(views.html.index())
   }
 
 }
