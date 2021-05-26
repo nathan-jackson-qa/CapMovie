dba29a6884b085a836cb550686d293fec9f7a228
“QAcpardeep” <cpardeep@qa.com>
2021-05-26 11:21:02 +0100

    cp: Committed search testing

--- CapMovies/test/controllers/HomeControllerTest.scala
+++ CapMovies/test/controllers/HomeControllerTest.scala
@@ -20,17 +20,22 @@ import persistence.connector.MovieConnector
 import persistence.domain.Movie
 import reactivemongo.bson.{BSONDocumentHandler, BSONObjectID}
 import test.AsyncAbstractTest
-
+import org.jsoup.Jsoup
 import java.net.URI
-import scala.concurrent.duration.DurationInt
+import scala.concurrent.duration.{Duration, DurationInt}
 import scala.language.postfixOps
 import scala.xml.Elem
+
+
 
 
 class HomeControllerTest extends AsyncAbstractTest {
 
   val mc = mock(classOf[MovieConnector])
   val controller = new HomeController(Helpers.stubControllerComponents(), mc)
+  def await[T](arg: Future[T]): T = Await.result(arg, Duration.Inf)
+  val movie = Movie(BSONObjectID.parse("609a678ce1a52451685d793f").get, "Gladiator", "Ridley Scott", "Russell Crowe", "R", "Action", "images/posters/gladiator.jpg")
+
 
   "HomeController" can {
 
@@ -65,6 +70,41 @@ class HomeControllerTest extends AsyncAbstractTest {
           x => assert(x.header.status.equals(200))
         }
       }
+    }
+
+    "Search" should {
+      "Search for a movie with no parameters" in {
+        when(mc.search(any())) thenReturn(Future.successful(Nil))
+
+        val result: Result = await(controller.search().apply(FakeRequest()))
+
+        result.header.status shouldBe 400
+
+      }
+      "Search for a movie with parameters" in {
+        when(mc.search(any())) thenReturn(Future.successful(Nil))
+
+        val result: Future[Result] = controller.search().apply(FakeRequest().withFormUrlEncodedBody("searchTerm" -> "Gladiator"))
+
+        await(result).header.status shouldBe 200
+
+        val doc = Jsoup.parse(contentAsString(result))
+
+        doc.getElementsByClass("card-body").size() shouldBe 0
+
+      }
+      "Search for a movie with parameter and returns a movie" in {
+        when(mc.search(any())) thenReturn(Future.successful(Seq(movie, movie)))
+
+        val result: Future[Result] = controller.search().apply(FakeRequest().withFormUrlEncodedBody("searchTerm" -> "Gladiator"))
+
+        await(result).header.status shouldBe 200
+
+        val doc = Jsoup.parse(contentAsString(result))
+
+        doc.getElementsByClass("card-body").size() shouldBe 2
+      }
+
     }
   }
 }
